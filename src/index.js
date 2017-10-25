f6c12d5890108c6a045545a9718b0b83c1dd52ae
byungjulee <okdoki9178@gmail.com>
2017-10-25 15:43:08 +0900

    프로젝트 초기화

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import ReactDOM from 'react-dom'
+
+
+class Home extends React.Component {
+    render() {
+        return (
+            <h2>Hey, I am HOME!</h2>
+        );
+    }
+}
+
+class About extends React.Component {
+    render() {
+        return (
+            <h2>Hey, I am ABOUT!</h2>
+        );
+    }
+}
+
+class Articles extends React.Component {
+    render() {
+        return (
+            <h2>Hey, I am ARTCILES!</h2>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+
+        return (
+                <div>
+                    <ul>
+                        <li><Link to="home">Home</Link></li>
+                        <li><Link to="about">About</Link></li>
+                        <li><Link to="articles">Articles</Link></li>
+                    </ul>
+                    {this.props.children}
+                </div>
+        );
+    }
+}
+  ReactDOM.render(<Router history = {browserHistory}>
+      <Route path = "/" component = {App}>
+         <IndexRoute component = {Home} />
+         <Route path = "home" component = {Home} />
+         <Route path = "about" component = {About} />
+         <Route path = "articles" component = {Articles} />
+      </Route>
+   </Router>, document.getElementById('app'));
